e75875a93929e65403151801e8d6cd0f8d54f959
verdaan <oyevardaan@gmail.com>
2022-05-19 02:02:00 +0530

    add skeleton for testing

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,5 @@
-// contract test code will go here
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+// create web3 instance with ganache provider
+const web3 = new Web3(ganache.provider());
